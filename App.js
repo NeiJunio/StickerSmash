9f7f3fd407e6f8b1b182789b87229153abf48190
Nei Junio Nogueira Gomes <152228340+NeiJunio@users.noreply.github.com>
2024-08-07 20:35:39 -0300

    commit

--- App.js
+++ App.js
@@ -1,24 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import { useState } from "react";
 
 import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
 
-const PlaceholderImage = require('./assets/images/background-image.png');
+import * as ImagePicker from 'expo-image-picker';
+
+const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
-    return (
-        <View style={styles.container}>
-            <View style={styles.imageContainer}>
-                <ImageViewer PlaceholderImageSource={PlaceholderImage} />
-            </View>
-            <View style={styles.footerContainer}>
-                <Button label="Choose a photo"></Button>
-                <Button label="Use this photo"></Button>
-            </View>
-            <StatusBar style="auto" />
+
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [showAppOptions, setShowAppOptions] = useState(false);
+
+    const pickImageAsync = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            allowsEditing: true,
+            quality: 1,
+        });
+
+        if (!result.canceled) {
+            setSelectedImage(result.assets[0].uri);
+            setShowAppOptions(true);
+        } else {
+            alert('You did not select image.')
+        }
+    };
+
+
+return (
+    <View style={styles.container}>
+        <View style={styles.imageContainer}>
+            <ImageViewer
+            placeholderImageSource={PlaceholderImage}
+            selectedImage={selectedImage}
+            />
         </View>
-    );
+        <View style={styles.footerContainer}>
+            <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+            <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
+        </View>
+        <StatusBar style="auto" />
+    </View>
+);
 }
 
 const styles = StyleSheet.create({
@@ -30,5 +55,9 @@ const styles = StyleSheet.create({
     imageContainer: {
         flex: 1,
         paddingTop: 58,
+    },
+    footerContainer: {
+        flex: 1 / 3,
+        alignItems: 'center',
     },
 });
